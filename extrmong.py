3bc06b5a050eb5444748ba786c519e36a32b0a4d
jw1123 <jw@jmbp.local>
2013-10-25 08:49:51 +0200

    Changes in extrmong.py and wavextrvis.py

--- extrmong.py
+++ extrmong.py
@@ -1,19 +1,26 @@
-from bregman.suite import MelFrequencyCepstrum, Chromagram, os, audio_dir
+from bregman.suite import MelFrequencyCepstrum, Chromagram, LinearPower, os, audio_dir
 import pymongo as pm
-from numpy import linalg, array
+from numpy import linalg, array, arange
 
 
-
+"""
 #Creating paths to the wav file (aduio_dir is the default path to the audio folder in the bregman folder)
 a1 = os.path.join(audio_dir,"chu.wav")
 a2 = os.path.join(audio_dir,"br.wav")
 
 #Calculates a MFC object for the wav file called by the respective path
-mfc1 = MelFrequencyCepstrum(a1, sample_rate=10000)
-mfc2 = MelFrequencyCepstrum(a2, sample_rate=10000)
+mfc1 = MelFrequencyCepstrum(a1, sample_rate=11000)
+mfc2 = MelFrequencyCepstrum(a2, sample_rate=11000)
 #Calculates a Chromogram object for the wav file called by the respective path
-chro1 = Chromagram(a1) #nfft=16384, wfft=8192, nhop=2205
-chro2 = Chromagram(a2) #nfft=16384, wfft=8192, nhop=2205
+chro1 = Chromagram(a1,sample_rate=11000) #nfft=16384, wfft=8192, nhop=2205
+chro2 = Chromagram(a2,sample_rate=11000) #nfft=16384, wfft=8192, nhop=2205
+
+
+linpow1 = LinearPower(a1,sample_rate=11000)
+linpow2 = LinearPower(a2,sample_rate=11000)
+
+
+
 
 #Access the MFCC array (numpy array)
 x = mfc1.MFCC
@@ -22,49 +29,87 @@ y = mfc2.MFCC
 #Access the CHROMA array (numpy array)
 p = chro1.CHROMA
 q = chro2.CHROMA
-
-
+"""
 
 
 if __name__ == "__main__":
 
 #_____________________________________SONG_COLLECTION_________________________________________#
-	con = pm.Connection()
-	db = con.test_db1 
-	song = db.song #Creating or accessing a collection named song
-	db.song.drop() #Clearing everything inside song
+    con = pm.Connection()
+    db = con.test_db1 
+    song = db.song #Creating or accessing a collection named song
+    #db.song.drop() #Clearing everything inside song
+    s = db.song.find(timeout=False)
+    r = db.song.find(timeout=False)
 
+    for so1 in s:
+        for so2 in r:
+            print so1['song_name'],so2['song_name']
+        r.rewind()
+    lis = []
+
+    for song in s:
+        lis.append(song['features'][0]['MFCC'])
+    a,b,c = [],[],[]
+    for i in arange(0,len(lis)):
+    	a +=lis[i]
+    for i in arange(0,len(a)):
+        b +=a[i]
+    print b[0:100]
+    print sum(b[0:100])/100
+    print len(b)
+    for i in arange(0,int(len(b)/100)):
+        try:
+            x = sum(b[i*100:(i+1)*100])/100
+        except:
+            x = sum(b[i*100:len(b)])/100
+        c.append(x)
+    print len(c)
+
+
+
+"""
 	#Inserting two songs into the song collection
 	#The MFCC and CHROMA arrays have to be stored as 1D lists accomplished by x.tolist()
-	song.insert({
-		'_id':1,
-		'song_name':'Can\'t Hold Us',
-		'album':'The Heist',
-		'features': [{'MFCC': x.tolist(), 'CHROMA':p.tolist()}], 
-		'artist':'Macklemore & Ryan Lewis',
-		'year':2012	
-		})
+    song.insert({
+        'id':1,
+        'song_name':'Can\'t Hold Us',
+        'album':'The Heist',
+        'features': [{
+        'MFCC': x.tolist(),
+        'CHROMA':p.tolist(),
+        'LIN_POW':linpow1.POWER.tolist()
+        }], 
+        'artist':'Macklemore & Ryan Lewis',
+        'year':2012	
+        })
 
-	song.insert({
-		'_id':2,
-		'song_name':'Bohemian Rhapsody',
-		'album':'A Night At The Opera',
-		'features': [{'MFCC': y.tolist(), 'CHROMA':q.tolist()}],
-		'artist':'Queen',
-		'year': 1975		
-		})
+    song.insert({
+        'id':2,
+        'song_name':'Bohemian Rhapsody',
+        'album':'A Night At The Opera',
+        'features': [{'MFCC': y.tolist(), 'CHROMA':q.tolist(),'LIN_POW':linpow2.POWER.tolist()}],
+        'artist':'Queen',
+        'year': 1975		
+        })
+"""
 
 
-	#s = db.song.find()
-	#for song in s:
+
+
+        #ft = song['features']
+        #mf = ft[0]
+        #print mf['CHROMA'][1]
+
 	#	print song['song_name'] #Print all the song names inside the song collection
 
 
 
 
 
-	#ft = s['features']
-	#mf = ft[0]
+    #ft = s['features']
+    #mf = ft[0]
+    #print mf
 	#print type(mf['MFCC'])
 	
 	#xf = pickle.loads(str(mf))
@@ -73,7 +118,7 @@ if __name__ == "__main__":
 
 	#col = s.collection
 	#print col
-
+"""
 
 #________________________________________SONG_COMPARISON____________________________________________#
 	distance = db.distance #Creating or accessing a collection named distance
@@ -93,7 +138,7 @@ if __name__ == "__main__":
 		y1 = ya.tolist() + ya.tolist()[0:len(xa.tolist())-len(ya.tolist())]
 		y1a = array(y1).reshape(x.shape[0], x.shape[1]) #Reshaping again to create an array of the initial dimensions
 		d1 = linalg.norm(x-y1a) #Calculates the distance
-	elif x.shape[1] < y.shape[1]:
+	else:
 		xa = x.reshape(x.shape[1],x.shape[0])
 		ya = y.reshape(y.shape[1],y.shape[0])
 		x1 = xa.tolist() + xa.tolist()[0:len(ya.tolist())-len(xa.tolist())]
@@ -110,7 +155,7 @@ if __name__ == "__main__":
 		q1 = qa.tolist() + qa.tolist()[0:len(pa.tolist())-len(qa.tolist())]
 		q1a = array(q1).reshape(p.shape[0], p.shape[1])
 		d2 = linalg.norm(p-q1a) #Calculates the distance
-	elif p.shape[1] < q.shape[1]:
+	else:
 		pa = p.reshape(p.shape[1],p.shape[0])
 		qa = q.reshape(q.shape[1],q.shape[0])
 		p1 = pa.tolist() + pa.tolist()[0:len(qa.tolist())-len(pa.tolist())]
@@ -120,11 +165,12 @@ if __name__ == "__main__":
 
 	distance.insert({
 		'_id': 1,
-		'id1': 1,
-		'id2': 2,
+		'source': 1, 
+		'target': 2, 
 		'MFCC': d1,
 		'CHROMA': d2
 		})
+
 
 
 
@@ -133,3 +179,4 @@ if __name__ == "__main__":
 	db.song.drop()
 	db.distance.remove()
 	db.distance.drop()
+"""
--- wavextrvis.py
+++ wavextrvis.py
@@ -5,29 +5,64 @@ Created on Wed Sep 18 09:43:59 2013
 @author: jw
 """
 
-from bregman.suite import LinearFrequencySpectrum, Chromagram, MelFrequencyCepstrum, os, audio_dir
+from bregman.suite import LinearFrequencySpectrum, MelFrequencySpectrum, LowQuefrencyLogFrequencySpectrum, Chromagram, LinearPower, HighQuefrencyMelSpectrum, MelFrequencyCepstrum, os, audio_dir
+#import numpy as np
 
 
+#a1 = os.path.join(audio_dir,"chu.wav")
+#a2 = os.path.join(audio_dir,"br.wav")
+#a3 = os.path.join(audio_dir,"ra.wav")
 
-a1 = os.path.join(audio_dir,"chu.wav")
-a2 = os.path.join(audio_dir,"br.wav")
-a3 = os.path.join(audio_dir,"ra.wav")
-
-
-
-linspec = LinearFrequencySpectrum(a1, nfft=1024, wfft=512, nhop=256)
-linspec.feature_plot(dbscale=True)
-title('Wide-band Linear Spectrum Cant Hold Us')
+a1 = "/Users/jw/Documents/Studium/EPFL/Internship/Vandergheynst/FEBr/Songs/cant_hold_us.wav"
 
 
-chro = Chromagram(a1) #nfft=16384, wfft=8192, nhop=2205
-chro.feature_plot(dbscale=True, normalize=True)
-title('Chromagram Cant Hold Us')
+#linspec = LinearFrequencySpectrum(a1, nfft=1024, wfft=512, nhop=256)
+#linspec.feature_plot(dbscale=True)
+#title('Wide-band Linear Spectrum Cant Hold Us')
+
+
+#chro = Chromagram(a1) #nfft=16384, wfft=8192, nhop=2205
+#chro.feature_plot(dbscale=True, normalize=True)
+#title('Chromagram Cant Hold Us')
+
+#linp = LinearPower(a1)
+#linp.feature_plot(dbscale=True, normalize=True)
+#title('Linear Power Cant Hold Us')
+
+#hcha = HighQuefrencyMelSpectrum(a1)
+#hcha.feature_plot(dbscale=True, normalize=True)
+#title('High freq mel spec Cant Hold Us')
+
 
 mfcc = MelFrequencyCepstrum(a1)
 mfcc.feature_plot(dbscale=True)
-title('MFCC Cant Hold Us')
+title('MFCC Cant Hold Us 1')
 
+
+a = mfcc.MFCC
+
+
+import numpy
+
+try:
+    from scipy.stats import nanmean as mean
+except ImportError:
+    import numpy.mean as mean
+
+def downsample(myarr,factor,estimator=mean):
+    ys,xs = myarr.shape
+    crarr = myarr[:ys-(ys % int(factor)),:xs-(xs % int(factor))]
+    dsarr = estimator( numpy.concatenate([[crarr[i::factor,j::factor] 
+        for i in range(factor)] 
+        for j in range(factor)]), axis=0)
+    return dsarr
+
+mfcc.MFCC = downsample(a,2)
+mfcc.feature_plot(dbscale=True)
+title('MFCC Cant Hold Us 3')
+
+
+"""
 
 linspec1 = LinearFrequencySpectrum(a2, nfft=1024, wfft=512, nhop=256)
 linspec1.feature_plot(dbscale=True)
@@ -70,7 +105,7 @@ rms1 = RMS(a3)
 rms1.feature_plot(dbscale=True)
 title('RMS Rachmaninoff')
 
-
+"""
 
 
 
