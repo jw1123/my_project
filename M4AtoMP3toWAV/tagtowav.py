263208b6dbd5b3a8c7d4a7a266f93f5d782536a5
Jonathan <jonathan@lts2mac9.epfl.ch>
2013-10-25 09:35:06 +0200

    Changes in antomp3.py and tagtowav.py

--- M4AtoMP3toWAV/tagtowav.py
+++ M4AtoMP3toWAV/tagtowav.py
@@ -1,30 +1,43 @@
 from mutagen.id3 import ID3
 from ffmpegwrapper import FFmpeg, AudioCodec, Input, Output
+import os
+from subprocess import call
 
 
-t = open('test.txt','r')
+#t = open('test.txt','r')
+
+i = 0
+
+for root,dirs,files in os.walk("/Users/jonathan/Desktop/MP3tag/"): #replace the path
+    for file1 in files:
+        i += 1
+        if file1[len(file1)-3:len(file1)] == "mp3":
+            a = 0
+            tag = ID3("/Users/jonathan/Desktop/MP3tag/"+file1)
+            input1 = Input("/Users/jonathan/Desktop/MP3tag/"+file1)
+            codec = AudioCodec("pcm_s16le")
+            print "____________________________"
+            print tag
+            try:
+                b = tag["TDRC"]
+                a = 1
+            except:
+                a = 0
+
+            if a == 1:
+                call(["ffmpeg", "-i", "/Users/jonathan/Desktop/MP3tag/"+file1, "-ar", "11000", "/Users/jonathan/Desktop/Wave1/"+str(tag["TIT2"])+"-*-"+str(tag["TPE1"])+"-*-"+str(tag["TALB"])+"-*-"+str(tag["TCON"])+"-*-"+str(tag["TDRC"])+".wav"])
+                #output = Output("/Users/jonathan/Desktop/Wave1/"+str(tag["TIT2"])+"-*-"+str(tag["TPE1"])+"-*-"+str(tag["TALB"])+"-*-"+str(tag["TCON"])+"-*-"+str(tag["TDRC"])+".wav", codec)
+            else:
+                call(["ffmpeg", "-i", "/Users/jonathan/Desktop/MP3tag/"+file1, "-ar", "11000", "/Users/jonathan/Desktop/Wave1/"+str(tag["TIT2"])+"-*-"+str(tag["TPE1"])+"-*-"+str(tag["TALB"])+"-*-"+str(tag["TCON"])+".wav"])
+                #output = Output("/Users/jonathan/Desktop/Wave1/"+str(tag["TIT2"])+"-*-"+str(tag["TPE1"])+"-*-"+str(tag["TALB"])+"-*-"+str(tag["TCON"])+".wav", codec)
+            #ffmpeg = FFmpeg("ffmpeg", input1, output)
+            #ffmpeg.add_parameter('-ar','22000')
+            #ffmpeg.run()
 
 
-for line in t:
-	#print typeline,
-	l = line[0:len(line)-1]
-	print l[len(l)-3:len(l)]
-	if l[len(l)-3:len(l)] == "m4a":
-		input1 = Input(l)
-		codec = AudioCodec("libmp3lame")
-		output = Output(l[0:len(l)-3]+"mp3", codec)
-		ffmpeg = FFmpeg("ffmpeg", input1, output)
-		ffmpeg.run()
 
-	if l[len(l)-3:len(l)] == "mp3":
-		tag = ID3(l)
-		input1 = Input(l)
-		codec = AudioCodec("pcm_s16le")
-		output = Output("/Users/jw/Desktop/Test/Wave/"+str(tag["TIT2"])+"-*-"+str(tag["TPE1"])+"-*-"+str(tag["TALB"])+".wav", codec)
-		ffmpeg = FFmpeg("ffmpeg", input1, output)
-		ffmpeg.run()
 
-t.close()
+#t.close()
 
 """
 # If you need the output at converting
